6ca1857a8d2572f7279476d11a9722d396f57f36
Liminghing <im349412@gmail.com>
2025-04-05 11:31:30 +0900

    edit ISSUE-01
    :Wq

--- app/src/main/java/com/jkweyu/test02/MainActivity.kt
+++ app/src/main/java/com/jkweyu/test02/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         // develop 작업 11:28
+        // ISSUE-01 작업 11:30
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
